a143b841fa0a53254f349c8c2962849163948e83
cxmarcelo <marcelo.c.b22@gmail.com>
2022-06-30 23:44:34 -0300

    Busca de produtos por nome e Categorias

---
+++ src/main/java/com/marcelo/cursomc/services/ProdutoService.java
@@ -0,0 +1,40 @@
+package com.marcelo.cursomc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.marcelo.cursomc.domain.Categoria;
+import com.marcelo.cursomc.domain.Produto;
+import com.marcelo.cursomc.repositories.CategoriaRepository;
+import com.marcelo.cursomc.repositories.ProdutoRepository;
+import com.marcelo.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository repo;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepo;
+	
+	public Produto find(Integer id) {
+		Optional<Produto> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
+	}
+	
+	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepo.findAllById(ids);
+		return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
+	}
+	
+}
