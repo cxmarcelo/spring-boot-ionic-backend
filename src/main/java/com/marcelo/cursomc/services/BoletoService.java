d74b02f33016e88182c93993a03249bddfb10e11
cxmarcelo <marcelo.c.b22@gmail.com>
2022-07-01 21:51:02 -0300

    Inserindo Pedido

---
+++ src/main/java/com/marcelo/cursomc/services/BoletoService.java
@@ -0,0 +1,22 @@
+package com.marcelo.cursomc.services;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import com.marcelo.cursomc.domain.PagamentoComBoleto;
+
+@Service
+public class BoletoService {
+
+	
+	//exemplo web service boleto
+	
+	public void preencherPagamentoComBoleto(PagamentoComBoleto pagto, Date instanteDoPedido) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(instanteDoPedido);
+		cal.add(Calendar.DAY_OF_MONTH, 7);
+		pagto.setDataVencimento(cal.getTime());
+	}
+}
